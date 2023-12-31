f96a49dd73dba934344916b6b0edb3d7862668fb
Minjae Kim <wnsqud7070@gmail.com>
2023-12-31 21:58:21 +0900

    레디스를 이용한 트랙픽 분산

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.example.api.service;
 
 import com.example.api.domain.Coupon;
+import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,15 +9,19 @@ import org.springframework.stereotype.Service;
 public class ApplyService {
     private final CouponRepository couponRepository;
 
-    public ApplyService(CouponRepository couponRepository) {
+    private final CouponCountRepository couponCountRepository;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
         this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
     }
 
-
     public void apply(Long userId) {
-        long count = couponRepository.count();
+        //long count = couponRepository.count();
+        Long count = couponCountRepository.increment(); // redis incr 명령어 (1증가)
 
-        if (count == 100) {
+        //100개넘으면 종료
+        if (count >100) {
             return;
         }
         couponRepository.save(new Coupon(userId));
